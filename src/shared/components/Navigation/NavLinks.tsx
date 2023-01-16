2234fc3a2c3a484bfe9f3c190ac694ee460bc653
william <william.steinkedemello@gmail.com>
2023-01-16 12:38:52 +0100

    context changes being listened

--- src/shared/components/Navigation/NavLinks.tsx
+++ src/shared/components/Navigation/NavLinks.tsx
@@ -1,10 +1,13 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { AuthContext } from "../../context/auth-context";
 import styles from "./NavLinks.module.css";
 
 interface NavLinksProps {}
 
 const NavLinks: FC<NavLinksProps> = () => {
+    const authCtx = useContext(AuthContext);
+
     return (
         <ul className={styles["nav-links"]}>
             <li>
@@ -16,32 +19,45 @@ const NavLinks: FC<NavLinksProps> = () => {
                     ALL USERS
                 </NavLink>
             </li>
-            <li>
-                <NavLink
-                    to="/639620c5842324dfdaa37c54/places"
-                    className={(isActive) => (isActive ? styles["active"] : "")}
-                >
-                    MY PLACES
-                </NavLink>
-            </li>
+            {authCtx.isLoggedIn && (
+                <li>
+                    <NavLink
+                        to="/639620c5842324dfdaa37c54/places"
+                        className={(isActive) =>
+                            isActive ? styles["active"] : ""
+                        }
+                    >
+                        MY PLACES
+                    </NavLink>
+                </li>
+            )}
 
-            <li>
-                <NavLink
-                    to="/places/new"
-                    className={(isActive) => (isActive ? styles["active"] : "")}
-                >
-                    ADD PLACE
-                </NavLink>
-            </li>
+            {authCtx.isLoggedIn && (
+                <li>
+                    <NavLink
+                        to="/places/new"
+                        className={(isActive) =>
+                            isActive ? styles["active"] : ""
+                        }
+                    >
+                        ADD PLACE
+                    </NavLink>
+                </li>
+            )}
 
-            <li>
-                <NavLink
-                    to="/auth"
-                    className={(isActive) => (isActive ? styles["active"] : "")}
-                >
-                    AUTHENTICATE
-                </NavLink>
-            </li>
+            {!authCtx.isLoggedIn && (
+                <li>
+                    <NavLink
+                        to="/auth"
+                        className={(isActive) =>
+                            isActive ? styles["active"] : ""
+                        }
+                    >
+                        AUTHENTICATE
+                    </NavLink>
+                </li>
+            )}
+
             <li>
                 <button>Log in</button>
             </li>
